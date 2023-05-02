2d859f676a9c6a3b6bca9666ad105d48a644908c
rownox <rownox.ik@gmail.com>
2023-05-02 17:38:40 -0400

    AlloPvP 2

---
+++ src/main/java/allo/net/allopvp/Allopvp.java
@@ -0,0 +1,17 @@
+package allo.net.allopvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Allopvp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
